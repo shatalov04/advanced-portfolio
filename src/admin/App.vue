2fcef81792c34107bbc2fadbd285b9034c3bf4ec
Maxim Shatalov <shatalov@pointcad.ru>
2020-05-16 13:44:43 +0300

    add yells

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -295,6 +295,104 @@
                         .button__caption Удалить
                         SvgIcon(className="button__icon button__icon_remove" name="remove")
 
+    section.yells#yells
+      .container.yells__container
+        .section-title.yells__title
+          h1.section-title__text Блок "{{sections[2].title}}"
+        .yells__content
+          .yells__form
+            form.form
+              .form__title Новый отзыв
+              .form__content
+                .form__loader.yells__loader
+                  .photo-loader
+                    button.button.button_user.yells__user-button(type="button")
+                      SvgIcon(className="button__icon button__icon_man-user" name="man-user")
+                      .button__caption Добавить фото
+                .form__properties
+                  .form-property
+                    .form-property__label Имя автора
+                    input.input.form-property__input(
+                      name="name"
+                      placeholder="Введите имя")
+                  .form-property
+                    .form-property__label Титул автора
+                    input.input.form-property__input(
+                      name="occupation"
+                      placeholder="Введите титул")
+                  .form-property
+                    .form-property__label Отзыв
+                    textarea.input.input_textarea.form-property__input(
+                      name="yell"
+                      placeholder="Введите отзыв")
+                  .form__buttons
+                    button(type="button").form__button.form__button_secondary Отмена
+                    button(type="submit").form__button сохранить
+
+          ul.section-list.yells__list
+            li.section-list__item.yells__item
+              .yells__new
+                button.button.button_add.yells__add-button(type="button")
+                  SvgIcon(className="button__icon button__icon_plus" name="plus")
+                  .button__caption Добавить отзыв
+            li.section-list__item.yells__item
+              .yell
+                .yell__header
+                  .user.yell__user
+                    .avatar.user__avatar
+                      img.avatar__img(src="../images/content/avatar-1.jpg")
+                    .user__desc
+                      .user__name Владимир Сабанцев
+                      .user__caption Преподаватель
+                .yell__info
+                  .yell__desc Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque pariatur eius, maiores repellat, sint veniam quibusdam odio tenetur ullam praesentium itaque alias assumenda quos totam fugit accusantium at voluptates eligendi id nemo sit voluptas magni, dolores magnam?
+                .yell__bar
+                  .control-bar
+                    button.button.button_edit.control-bar__button(type="button")
+                      .button__caption Редактировать
+                      SvgIcon(className="button__icon button__icon_edit" name="pencil")
+                    button.button.button_remove.control-bar__button(type="button")
+                      .button__caption Удалить
+                      SvgIcon(className="button__icon button__icon_remove" name="remove")
+            li.section-list__item.yells__item
+              .yell
+                .yell__header
+                  .user.yell__user
+                    .avatar.user__avatar
+                      img.avatar__img(src="../images/content/avatar-1.jpg")
+                    .user__desc
+                      .user__name Ковальчук Дмитрий
+                      .user__caption Основатель Loftschool
+                .yell__info
+                  .yell__desc Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                .yell__bar
+                  .control-bar
+                    button.button.button_edit.control-bar__button(type="button")
+                      .button__caption Редактировать
+                      SvgIcon(className="button__icon button__icon_edit" name="pencil")
+                    button.button.button_remove.control-bar__button(type="button")
+                      .button__caption Удалить
+                      SvgIcon(className="button__icon button__icon_remove" name="remove")
+            li.section-list__item.yells__item
+              .yell
+                .yell__header
+                  .user.yell__user
+                    .avatar.user__avatar
+                      img.avatar__img(src="../images/content/avatar-1.jpg")
+                    .user__desc
+                      .user__name Владимир Сабанцев
+                      .user__caption Преподаватель
+                .yell__info
+                  .yell__desc Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque pariatur eius, maiores repellat, sint veniam quibusdam odio tenetur ullam praesentium itaque alias assumenda quos totam fugit accusantium at voluptates eligendi id nemo sit voluptas magni, dolores magnam?
+                .yell__bar
+                  .control-bar
+                    button.button.button_edit.control-bar__button(type="button")
+                      .button__caption Редактировать
+                      SvgIcon(className="button__icon button__icon_edit" name="pencil")
+                    button.button.button_remove.control-bar__button(type="button")
+                      .button__caption Удалить
+                      SvgIcon(className="button__icon button__icon_remove" name="remove")
+
 </template>
 
 <script>
@@ -384,12 +482,17 @@ export default {
   width: 46px;
   margin-right: 20px;
 }
+.user__desc {
+  display: flex;
+  flex-direction: column;
+}
 .user__name {
   font-size: 18px;
   font-weight: 600;
   padding-bottom: 2px;
 }
-
+.user__caption {
+}
 //avatar
 .avatar {
   overflow: hidden;
@@ -584,6 +687,7 @@ export default {
 //input
 .input {
   border: 0;
+  font-weight: 600;
   width: 100%;
   border-bottom: 1px solid $text-color;
   padding-right: 15px;
@@ -598,7 +702,7 @@ export default {
     padding-left: 15px;
   }
   &_title {
-    font-weight: 600;
+    font-size: 18px;
     padding-bottom: 15px;
   }
   &_percentage {
@@ -687,8 +791,6 @@ export default {
   }
 }
 .works__form {
-  background: white;
-  box-shadow: var(--form-boxshadow);
 }
 
 .works__list {
@@ -708,6 +810,8 @@ export default {
 
 //form
 .form {
+  background: white;
+  box-shadow: var(--form-boxshadow);
 }
 .form__title {
   font-size: 18;
@@ -741,6 +845,9 @@ export default {
 }
 
 .form__buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
 }
 .form__button {
   color: white;
@@ -862,5 +969,123 @@ export default {
   & .button__icon_remove {
     color: $admin-error-color;
   }
+}
+
+//**********************************ABOUT***************************************/
+//**********************************ABOUT***************************************/
+//**********************************ABOUT***************************************/
+//**********************************ABOUT***************************************/
+
+//yells
+.yells {
+  background: $admin-bg-color;
+  padding-bottom: 50px;
+}
+//prettier-ignore
+.yells__container {
+  width: 1200Px;
+}
+.yells__title {
+  padding: 60px 0;
+}
+.yells__content {
+}
+.yells__form {
+}
+.yells__loader {
+  background: white;
+  width: 230px;
+}
+.yells__list {
+  margin-right: -20px;
+  margin-top: 20px;
+}
+.yells__item {
+  width: calc((100% / 3) -20px);
+  margin-right: 20px;
+  margin-bottom: 20px;
+  background: white;
+  box-shadow: var(--form-boxshadow);
+}
+.yells__user-button {
+  flex-direction: column;
+}
+.yells__add-button {
+  flex-direction: column;
+}
+.yells__new {
+  height: 100%;
+  width: 100%;
+  background: $links-color;
+  background: var(--bg-gradient);
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & .button__icon_plus {
+    width: 150px;
+    height: 150px;
+    padding: 64px;
+    border: 2px solid white;
+    color: white;
+    background: transparent;
+    margin-bottom: 20px;
+  }
+  & .button__caption {
+    width: 50%;
+    font-weight: 600;
+    color: white;
+    line-height: 30px;
+  }
+}
+
+//photo-loader
+.photo-loader {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & .button__icon_man-user {
+    overflow: hidden;
+    border-radius: 50%;
+    width: 200px;
+    height: 200px;
+    padding: 15px;
+    color: white;
+    background: $admin-loader-color;
+    margin-bottom: 20px;
+  }
+  & .button__caption {
+    font-weight: 600;
+    color: $links-color;
+    line-height: 30px;
+  }
+}
+
+//work
+.yell {
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.yell__header {
+}
+.yell__user {
+  margin-bottom: 20px;
+}
+.yell__info {
+}
+.yell__desc {
+  margin-bottom: 20px;
+}
+.yell__bar {
+  margin-top: auto;
+}
+
+//control-bar
+.control-bar {
 }
 </style>
